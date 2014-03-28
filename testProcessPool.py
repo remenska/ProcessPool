62a402395c1cd8716871bbbac1058ad6dc88b830
Daniela <remenska@gmail.com>
2014-03-28 15:13:35 +0100

    Tja

---
+++ testProcessPool.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+import random
+import time
+
+
+class RequestTask:
+  def __init__(self, id):
+    self.__id = id
+    print "Create %s"%id
+
+  def __call__(self):
+    sleepTime = random.randint(0,3)
+    print "Req %s sleep %s"%(self.__id, sleepTime)
+    time.sleep(sleepTime)
+    print "Req %s finished"%self.__id
+
+
+from ProcessPool import ProcessPool
+
+def resultCallback( self, taskID, taskResult ):
+  print "callback taskID %s result %s"%(taskID, taskResult)
+
+def exceptCallback( self, taskID, taskResult ):
+  print "except callback taskID %s result %s"%(taskID, taskResult)
+
+random.seed(0)
+
+__processPool = None
+
+def processPool(  ):
+  """ facade for ProcessPool """
+  global __processPool
+  if not __processPool:
+
+    __processPool = ProcessPool( 1,
+                                      8,
+                                      10,
+                                      poolCallback = resultCallback,
+                                      poolExceptionCallback = exceptCallback )
+    __processPool.daemonize()
+  return __processPool
+
+
+print "processPool tasks idle = %s working = %s" % ( processPool().getNumIdleProcesses(), processPool().getNumWorkingProcesses() )
+
+idPool = range(10)
+retried = 0
+
+while True:
+  reqId = 0
+  try:
+    reqId = idPool.pop()
+  except:
+    if retried== 1 :
+      break
+    retried += 1
+    print "Empty, we wait"
+    time.sleep(25)
+    idPool = range(10)
+    continue
+
+  if not processPool().getFreeSlots():
+    print "No free slots available in processPool, will wait %d seconds to proceed" % 1
+    time.sleep( 1 )
+  else:
+    print "spawning task for request %s"%reqId
+    enqueue = processPool().createAndQueueTask( RequestTask,
+	                                           kwargs = { "id": reqId}, 
+	                                           taskID = reqId,
+	                                           blocking = True,
+	                                           usePoolCallbacks = True,
+	                                           timeOut = 9 )
+
+processPool().finalize( timeout = 5 )
+
+
+
